08d0b5f87bab817c745d0622c5f81f14bfb8397a
Dane Smith <doctorhowser@gmail.com>
2018-01-17 14:25:43 -0600

    add missing files

---
+++ server/public/scripts/controllers/random.controller.js
@@ -0,0 +1,11 @@
+giphyApp.controller('RandomController', function () {
+  console.log('RandomController started.');
+  var vm = this;
+  vm.searchResults = [];
+
+  // Search for random giphy images
+  vm.randomSearch = function() {
+      // YOUR CODE HERE
+  }
+
+});
