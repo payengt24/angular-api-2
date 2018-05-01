92fa8565bbf2c75d90433f469cb7709fe69685a8
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-05-01 11:00:35 -0500

    files initiated for SPA (empty)

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,6 @@
 console.log('client.js');
 
-const app = angular.module('GiphyApp', []);
+const app = angular.module('GiphyApp', ['ngRoute']);
 const myKey = 'nr7lnegD2M71DZLZ5KcLHipXbA3bpMu6';
 
 
@@ -9,17 +9,21 @@ app.controller('GiphyController', ['$http', function($http) {
   var self = this;
   self.src = '';
   self.searchIn = '';
-  
+
 
   self.getSearch = function () {
     $http({
       method: 'GET',
       url: 'https://api.giphy.com/v1/gifs/search',
-      params: {api_key: myKey, q: self.searchIn, limit: 1},
+      params: {
+        api_key: myKey, 
+        q: self.searchIn, 
+        limit: 1
+      },
       
     })
       .then(function(response) {
-        console.log(response.data.data);
+        console.log(response.data.data[0].images.original);
         self.src = response.data.data[0].images.original.url;
       })
       .catch(function(error) {
